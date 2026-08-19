8a3c0cd7c20cdf8f231182061d8e14af04a71c1f
AbineshS4579 <boopathiraja181721@gmail.com>
2026-08-19 22:33:49 +0530

    Happy number

---
+++ happy.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class happy  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while (n != 1 && n != 4) {
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        if (n == 1)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
