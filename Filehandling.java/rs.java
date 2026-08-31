e33e85142c0f4cd0564e2d2c412728c49d44e3d8
AbineshS4579 <boopathiraja181721@gmail.com>
2026-08-31 17:36:30 +0530

    Reverse Words in a String

---
+++ Filehandling.java/rs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class rs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+
+        System.out.println(result);
+    }
+}
