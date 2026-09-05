88669f840c30133ce977de20846c2e01fb1076eb
AbineshS4579 <boopathiraja181721@gmail.com>
2026-09-05 21:58:44 +0530

    Calculate Circle Area

---
+++ Filehandling.java/cir.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class cir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double r = sc.nextDouble();
+
+        double area = Math.PI * r * r;
+
+        System.out.println(area);
+    }
+}
