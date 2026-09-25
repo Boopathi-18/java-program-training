6f70ff6318dcacd396940ab95e50681ef9d3fde0
AbineshS4579 <boopathiraja181721@gmail.com>
2026-09-25 23:07:26 +0530

    Reverse String

---
+++ Filehandling.java/rsfan.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class rsfan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println(new String(arr));
+    }
+}
